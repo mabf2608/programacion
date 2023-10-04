51cf14acf4ef6e9bed10e4d3c042df541300d7e9
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-04 20:39:07 +0200

    Ejercicio 3 Resuelto con anotaciones

---
+++ CAP01EJ01/src/Ejercicio3.java
@@ -0,0 +1,14 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        System.out.printf("%-10s%-10s%n","computer","ordenador");//Para alinear a la izquierda usamos el %-10 por decir un número de caracteres(ancho)
+        System.out.printf("%-10s%-10s%n","student","alumno/a");//Para insertar el salto de página usamos el %n
+        System.out.printf("%-10s%-10s%n","cat","gato");//La s nos indica el valor de los datos que se van a introducir, en este caso una String
+        System.out.printf("%-10s%-10s%n","penguin","pingüino");
+        System.out.printf("%-10s%-10s%n","machine","maquina");
+        System.out.printf("%-10s%-10s%n","nature","naturaleza");
+        System.out.printf("%-10s%-10s%n","light","luz");
+        System.out.printf("%-10s%-10s%n","green","verde");
+        System.out.printf("%-10s%-10s%n","book","libro");
+        System.out.printf("%-10s%-10s%n","pyramid","pirámide");
+    }
+}
