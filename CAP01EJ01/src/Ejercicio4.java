7cb2ae49cc8f582b3544b05ad57ed037ead44dff
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-04 21:37:44 +0200

    Ejercicio 4 resuelto

---
+++ CAP01EJ01/src/Ejercicio4.java
@@ -0,0 +1,17 @@
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.printf("|%-11s|%10s|%10s|%13s|%10s|%10s|%n", "Horas/Dias","Lunes","Martes","Miércoles","Jueves","Viernes");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.printf("|%-10s|%10s|%10s|%13s|%10s|%10s|%n", "09:15-10:15","PROGR","LMSGI","LMSGI","PROGR","FOL");
+        System.out.printf("|%-10s|%10s|%10s|%13s|%10s|%10s|%n", "09:15-10:15","PROGR","LMSGI","LMSGI","PROGR","FOL");
+        System.out.printf("|%-10s|%10s|%10s|%13s|%10s|%10s|%n", "10:15-11:15","PROGR","BADAT","PROGR","PROGR","FOL");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.printf("|%-10s|%10s|%10s|%13s|%10s|%10s|%n", "11:15-11:45","R","E","CR","E","O");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.printf("|%-10s|%10s|%10s|%13s|%10s|%10s|%n", "11:45-12:45","SIINF","BADAT","PROGR","BADAT","SIINF");
+        System.out.printf("|%-10s|%10s|%10s|%13s|%10s|%10s|%n", "12:45-13:45","SIINF","ENDES","BADAT","BADAT","SIINF");
+        System.out.printf("|%-10s|%10s|%10s|%13s|%10s|%10s|%n", "13:45-14:45","SIINF","ENDES","BADAT","ENDES","SIINF");
+        System.out.println("-----------------------------------------------------------------------");
+    }
+}
