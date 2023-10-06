ddefb4c19c11a10eee8abd5651349eecf34bbf0c
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-06 18:36:05 +0200

    Ejercicio 3 Tema 3

---
+++ CAP03EJS/src/Ejercicio3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce la cantidad de pesetas que quieras pasar a euros: ");
+        int pesetas = sc.nextInt();
+        float euros = (float)(pesetas/166);
+
+        System.out.printf("%d pesetas son aproximadamente %.2f €", pesetas,euros);
+        sc.close();
+    }
+}
