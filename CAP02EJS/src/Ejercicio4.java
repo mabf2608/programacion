4e2a848ef8d084841c510734443f5c9a5f5f151e
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-06 19:02:49 +0200

    Corrección Ejercicios 4 y 5 Tema 2

--- CAP02EJS/src/Ejercicio4.java
+++ CAP02EJS/src/Ejercicio4.java
@@ -1,13 +1,8 @@
-import java.util.Scanner;
-
 public class Ejercicio4 {
     public static void main(String[] args) {
-    Scanner leer = new Scanner(System.in);
-    System.out.print("Introduce la cantidad de euros que quieres pasar a pesetas: ");
-        float euros = leer.nextFloat();
+        float euros = 537;
         int pesetas = (int)(euros*166);
     System.out.printf("%.2f € en pesetas son aproximadamente: %d.", euros, pesetas);
 
-    leer.close();
     }
 }
--- CAP02EJS/src/Ejercicio5.java
+++ CAP02EJS/src/Ejercicio5.java
@@ -1,13 +1,8 @@
-import java.util.Scanner;
-
 public class Ejercicio5 {
     public static void main(String[] args) {
-    Scanner leer = new Scanner(System.in);
-    System.out.print("Introduce la cantidad de pesetas que quieres pasar a euros: ");
-        int pesetas = leer.nextInt();
+        int pesetas = 89142;
         float euros = pesetas/166;
     System.out.printf("%d pesetas en euros son aproximadamente: %.2f €.",pesetas, euros);
 
-    leer.close();
     }
 }
