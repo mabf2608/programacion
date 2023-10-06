09d4c4d1ea584c17bf71d3db21faffa7874e954c
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-06 18:55:40 +0200

    Ejercicios 5 y 6 Tema 3

--- CAP03EJS/src/Ejercicio6.java
+++ CAP03EJS/src/Ejercicio6.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class Ejercicio5 {
+public class Ejercicio6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce la altura del rectángulo: ");
+        System.out.println("Introduce la altura del triángulo: ");
         int altura = sc.nextInt();
 
-        System.out.println("Introduce la base del rectángulo: ");
+        System.out.println("Introduce la base del triángulo: ");
         int base = sc.nextInt();
 
         System.out.println();
-        int area = base*altura;
+        int area = (base*altura)/2;
         System.out.println("El área del rectángulo es: "+area);
         sc.close();
     }
