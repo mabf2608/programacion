6a3bb2b01e5b9cd04784a358a93634715724b50c
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-09 09:18:34 +0200

    Ejercicio10 Tema 1

---
+++ CAP01EJS/src/Ejercicio10.java
@@ -0,0 +1,25 @@
+public class Ejercicio10 {
+        public static final String BLACK = "\033[0;30m";   // BLACK
+        public static final String RED = "\033[0;31m";     // RED
+        public static final String GREEN = "\033[0;32m";   // GREEN
+        public static final String YELLOW = "\033[0;33m";  // YELLOW
+        public static final String BLUE = "\033[0;34m";    // BLUE
+        public static final String PURPLE = "\033[0;35m";  // PURPLE
+        public static final String CYAN = "\033[0;36m";    // CYAN
+        public static final String WHITE = "\033[0;37m";   // WHITE
+        public static final String RESET = "\033[0m";  // Text Reset
+        public static void main(String[] args) {
+        System.out.println();
+        System.out.printf("%32s%1s%n",RED+"\u2588","\u2588"+RESET);
+        System.out.printf("%23s%7s%1s%n",RED+"APRENDE JAVA","\u2588","\u2588"+RESET);
+        System.out.printf("%24s%8s%1s%n",RED+"CON EJERCICIOS","\u2588","\u2588"+RESET);
+        System.out.printf("%n");
+        System.out.printf("%29s%10s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%n",YELLOW+"Miguel A Bonilla"+RESET,CYAN+"\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588"+RESET);
+        System.out.printf("%28s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%n",CYAN+"\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","  ","\u2588","\u2588"+RESET);
+         System.out.printf("%28s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%n",CYAN+"\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588"+RESET);
+         System.out.printf("%28s%1s%1s%1s%1s%1s%1s%1s%1s%n",CYAN+"\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588"+RESET);
+         System.out.printf("%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%n",BLUE+"\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588"+RESET,CYAN+"\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588"+RESET,BLUE+"\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588"+RESET);
+        System.out.printf("%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%n",BLUE+"\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588"+RESET);
+        System.out.printf("%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%1s%n",BLUE+"\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588","\u2588"+RESET);
+        }
+    }
