64e59258e943974bc96be0a4a8c285de8f5441cb
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-12 19:09:12 +0200

    Ejercicio 9 Tema 4

---
+++ CAP04EJS/src/Ejercicio9.java
@@ -0,0 +1,34 @@
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.print("Este programa nos permite resolver una ecuación de segundo grado");
+        System.out.println();
+
+        System.out.print("Introduce el valor de a: ");
+        float a = Float.parseFloat(System.console().readLine());
+
+        System.out.print("Introduce el valor de b: ");
+        float b = Float.parseFloat(System.console().readLine());
+
+        System.out.print("Introduce el valor de c: ");
+        float c = Float.parseFloat(System.console().readLine());
+
+        float ec1 = (float)(-b + (Math.sqrt(Math.pow(b,2)-4*a*c)))/(2*a);
+        float ec2 = (float)(-b - (Math.sqrt(Math.pow(b,2)-4*a*c)))/(2*a);
+        System.out.println();
+
+        if ((Float.isNaN(ec1)) && (Float.isNaN(ec2))){
+            System.out.println("x = No tiene resultado.");
+        }else{
+        if (ec1 != ec2){
+            System.out.println("Tiene dos soluciones reales: ");
+            System.out.println("x1 = "+ec1);
+            System.out.println("x2 = "+ec2);
+        }else{
+            if (ec1 == ec2){
+            System.out.println("x = "+ ec1);
+                }
+            }
+        }
+    }
+}
