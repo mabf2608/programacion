f3c81d22a1b25d68446fbe33acd2470607315dc6
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-12 19:43:20 +0200

    Ejercicio 10 Tema 4

---
+++ CAP04EJS/src/Ejercicio10.java
@@ -0,0 +1,57 @@
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        System.out.print("Introduce el día en el que naciste: ");
+        int dia = Integer.parseInt(System.console().readLine());
+
+        System.out.print("Introduce el mes en el que naciste: ");
+        int mes = Integer.parseInt(System.console().readLine());
+
+        if (((dia >= 21 && dia <= 31 ) && mes == 3) || ((dia >= 0 && dia <= 19 ) && mes == 4)){
+            System.out.println("ERES ARIES.");
+        }else{
+            if (((dia >= 20 && dia <= 30 ) && mes == 4) || ((dia >= 0 && dia <= 20 ) && mes == 5)){
+            System.out.println("ERES TAURO.");
+        }else{
+            if (((dia >= 21 && dia <= 31 ) && mes == 5) || ((dia >= 0 && dia <= 20 ) && mes == 6)){
+            System.out.println("ERES GÉMINIS.");
+        }else{
+            if (((dia >= 21 && dia <= 30 ) && mes == 6) || ((dia >= 0 && dia <= 22 ) && mes == 7)){
+            System.out.println("ERES CÁNCER.");
+        }else{
+            if (((dia >= 23 && dia <= 31 ) && mes == 7) || ((dia >= 0 && dia <= 22 ) && mes == 8)){
+            System.out.println("ERES LEO.");
+        }else{
+            if (((dia >= 23 && dia <= 31 ) && mes == 8) || ((dia >= 0 && dia <= 22 ) && mes == 9)){
+            System.out.println("ERES VIRGO.");
+        }else{
+            if (((dia >= 23 && dia <= 30 ) && mes == 9) || ((dia >= 0 && dia <= 22 ) && mes == 10)){
+            System.out.println("ERES LIBRA.");
+        }else{
+            if (((dia >= 23 && dia <= 31 ) && mes == 10) || ((dia >= 0 && dia <= 21 ) && mes == 11)){
+            System.out.println("ERES ESCORPIO.");
+        }else{
+            if (((dia >= 22 && dia <= 30 ) && mes == 11) || ((dia >= 0 && dia <= 21 ) && mes == 12)){
+            System.out.println("ERES SAGITARIO.");
+        }else{
+            if (((dia >= 22 && dia <= 31 ) && mes == 12) || ((dia >= 0 && dia <= 19 ) && mes == 1)){
+            System.out.println("ERES CAPRICORNIO.");
+        }else{
+            if (((dia >= 20 && dia <= 31 ) && mes == 1) || ((dia >= 0 && dia <= 18 ) && mes == 2)){
+            System.out.println("ERES ACUARIO.");
+        }else{
+            if (((dia >= 19 && dia <= 29 ) && mes == 2) || ((dia >= 0 && dia <= 22 ) && mes == 3)){
+            System.out.println("ERES PISCIS.");
+             }
+            }
+           }
+          }
+         }
+        }
+       }
+      }
+     }
+    }
+   }
+  }
+ }
+}
