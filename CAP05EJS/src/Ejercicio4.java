ba27b3c3b21239e89c578710daacb3c3f24ee23a
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-22 21:48:25 +0200

    Ejercicio 4 Tema 5

---
+++ CAP05EJS/src/Ejercicio4.java
@@ -0,0 +1,8 @@
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        System.out.println();
+        for(int i = 320;i >= 160;i = i - 20){
+            System.out.print(i+" ");
+        }
+    }
+}
