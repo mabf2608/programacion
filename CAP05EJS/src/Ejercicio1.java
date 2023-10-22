b73f955466e8c7b2101dd74796ddc6525e766de7
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-22 21:44:39 +0200

    Ejercicios 1 al 3 Tema 5

--- CAP05EJS/src/Ejercicio1.java
+++ CAP05EJS/src/Ejercicio1.java
@@ -1,12 +1,8 @@
 public class Ejercicio1 {
-    public static void mult(){
-
-        for (int i=0;i<=100;i=i+5){
+    public static void main(String[] args) {
+        System.out.println();
+        for(int i=0;i<=100;i=i+5){
             System.out.print(i+" ");
         }
-    }
-    
-    public static void main(String[] args) {
-        mult();
     }
 }
---
+++ CAP05EJS/src/Ejercicio2.java
@@ -0,0 +1,10 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println();
+        int i = 0;
+        while (i <= 100){
+        System.out.print(i+" ");
+        i=i+5;
+        }
+    }
+}
