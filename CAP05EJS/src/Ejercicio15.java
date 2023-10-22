c821c0d6ccbc512d8dd1c70983f71e7fefaedd29
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-23 01:29:28 +0200

    Ejercicio 15 Tema 5

---
+++ CAP05EJS/src/Ejercicio15.java
@@ -0,0 +1,15 @@
+public class Ejercicio15 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Introduce una base: ");
+        int b = Integer.parseInt(System.console().readLine());
+        System.out.println("Introduce un exponente: ");
+        int e = Integer.parseInt(System.console().readLine());
+        int n = 0;
+        System.out.println();
+        for (int i = 1; i <= e; i++){
+            n = n+1; 
+            System.out.printf("%d^%d ",b,n);
+        }
+   }
+}
