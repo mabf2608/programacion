603b39cdce74f6bf59fb54d57900ff26597a1600
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-22 22:08:37 +0200

    Ejercicio 6 Tema 5

---
+++ CAP05EJS/src/Ejercicio6.java
@@ -0,0 +1,11 @@
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        System.out.println();
+        int i = 320;
+        do{
+            System.out.print(i + " ");
+            i = i - 20;
+        }
+        while(i >= 160);
+    }
+}
