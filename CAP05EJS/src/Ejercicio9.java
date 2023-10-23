23776307c61a7298afc093f4e0221a7bf5883a6f
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-23 09:38:26 +0200

    Edición ejercicio 9 Tema 5

--- CAP05EJS/src/Ejercicio9.java
+++ CAP05EJS/src/Ejercicio9.java
@@ -1,13 +1,17 @@
 public class Ejercicio9 {
     public static void main(String[] args) {
+        try{
         System.out.println();
         System.out.print("Introduce un número y te diré cuántas dígitos tiene: ");
         long n = Long.parseLong(System.console().readLine());
         int i = 0;
-        while (n > 0){
+        while (n > 0 || n < 0){
             n = n / 10;
             i = i + 1;
         }
         System.out.println("Tu número tiene "+i+" dígitos.");
+        }catch(Exception e){
+            System.out.println("ERROR: Número no permitido (demasiado largo)");
+        }
     }
 }
