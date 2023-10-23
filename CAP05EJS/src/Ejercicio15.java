5f27bbf0ca24e90dc47797ea776b77928fcbbae7
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-10-23 08:29:46 +0200

    Edición ejercicio 15 Tema 5

--- CAP05EJS/src/Ejercicio15.java
+++ CAP05EJS/src/Ejercicio15.java
@@ -7,9 +7,13 @@ public class Ejercicio15 {
         int e = Integer.parseInt(System.console().readLine());
         int n = 0;
         System.out.println();
+        if (e >=0){
         for (int i = 1; i <= e; i++){
             n = n+1; 
             System.out.printf("%d^%d ",b,n);
         }
+    }else{
+        System.out.println("ERROR: El exponente tiene que ser positivo.");
+    }
    }
 }
