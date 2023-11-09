b40d6f935cba91e7015d0b3ccfd87053793aea0f
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-11-09 08:43:01 +0100

    Ejercicio 44 Completo Tema 5

---
+++ CAP05EJS/src/Ejercicio44.java
@@ -0,0 +1,63 @@
+/*
+*Ejercicio 44 | Tema 5
+*Numero partido
+*@author Miguel Ángel Bonilla Fernández 1ºDAM
+*/
+public class Ejercicio44 {
+    public static String digitos(long v1){
+        boolean seguir = true;
+        double p = v1;
+        int i=0;
+        String dato="";
+        while (seguir){
+            i=(int)(p%10);
+            p=p/10;
+            dato=dato+i;
+            if (p<1){
+                seguir=false;
+            }
+        }
+        return dato;
+    }
+    public static void main(String[] args) {
+        System.out.println();
+        boolean seguir=true;
+        int p=0, d=0;
+        long n=0;
+        while(seguir){
+            System.out.println("Introduce un número entero positivo: ");
+            n = Long.parseLong(System.console().readLine());
+            if(n>0){
+                seguir=false;
+            }
+        }
+        seguir=true;
+        while(seguir){
+            System.out.println("Introduzca la posición donde quieras insertar: ");
+            p = Integer.parseInt(System.console().readLine());
+            if(p>0){
+                seguir=false;
+            }
+        }
+        seguir=true;
+        while(seguir){
+            System.out.println("Introduce el dígito a ingresar: ");
+            d = Integer.parseInt(System.console().readLine());
+            if(d>0){
+                seguir=false;
+            }
+        }
+        String dato=digitos(n);
+        int l = dato.length();
+        n=Long.parseLong(dato);
+        int i = 0, j = 0;
+        System.out.print("El número resultante es: ");
+        for (i=1;i<=l;i++){
+            j=(int)n%10;
+            n=n/10;
+            if (i==p)
+                System.out.print(d);
+            System.out.print(j);
+        }
+    }
+}
