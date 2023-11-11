fe02c6ec6bf40d89d910f424f2d54fc18e81ec4c
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-11-11 20:33:25 +0100

    Ejercicio 53 Completo Tema 5

---
+++ CAP05EJS/src/Ejercicio53.java
@@ -0,0 +1,19 @@
+/*
+*Ejercicio 53 | Tema 5
+*Triangulo relleno invertido
+*@author Miguel Ángel Bonilla Fernández 1ºDAM
+*/
+public class Ejercicio53 {
+     public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Introduzca la altura de la figura: ");
+            int a=Integer.parseInt(System.console().readLine());
+        int i=0, j=0;
+        for (i=0;i<=a;i++){
+            System.out.println();
+            for(j=1;j<=a-i;j++){
+                System.out.print("*");
+            }
+        }
+     }
+}
