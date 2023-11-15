5f8741a8c7073358b929544660ac63ca7e37be1a
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-11-15 09:17:45 +0100

    Ejercicio11 Tema 6

---
+++ CAP06EJS/src/Ejercicio11.java
@@ -0,0 +1,45 @@
+/*
+*Ejercicio 10 | Tema 6
+*Líneas de caracteres
+*@author Miguel Ángel Bonilla Fernández 1ºDAM
+*/
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        System.out.println();
+        int i=0,n=0,sus=0,suf=0,bi=0,not=0,sob=0;
+        String nota ="";
+        for (i=1;i<=20;i++){
+            n=(int)(Math.random()*5d);
+            switch (n) {
+                case 0:
+                    nota="Suspenso";
+                    sus++;
+                break;
+                case 1:
+                    nota="Suficiente";
+                    suf++;
+                break;
+                case 2:
+                    nota="Bien";
+                    bi++;
+                break;
+                case 3:
+                    nota="Notable";
+                    not++;
+                break;
+                case 4:
+                    nota="Sobresaliente";
+                    sob++;
+                break;
+                default:
+            }
+            System.out.printf("El alumno nº %d ha sacado un %s.%n",i,nota);
+        }
+        System.out.println();
+        System.out.printf("Ha habido un total de %d de Suspensos.%n",sus);
+        System.out.printf("Ha habido un total de %d de Suficientes.%n",suf);
+        System.out.printf("Ha habido un total de %d de Bien.%n",bi);
+        System.out.printf("Ha habido un total de %d de Notables.%n",not);
+        System.out.printf("Ha habido un total de %d de Sobresalientes.%n",sob);
+    }
+}
