cda6882c4ce7d4c862206b3b8a9109e31547fe2f
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-11-15 10:52:48 +0100

    Ejercicio1 Tema6 Edicición

--- CAP06EJS/src/Ejercicio1.java
+++ CAP06EJS/src/Ejercicio1.java
@@ -5,14 +5,12 @@
 */
 public class Ejercicio1 {
     public static void main(String[] args) {
-        int dado1=(int)(Math.random()*6)+1;
-        int dado2=(int)(Math.random()*6)+1;
-        int dado3=(int)(Math.random()*6)+1;
-        int suma=dado1+dado2+dado3;
-        System.out.println();
-        System.out.printf("En el dado 1 te ha salido: %d%n",dado1);
-        System.out.printf("En el dado 2 te ha salido: %d%n",dado2);
-        System.out.printf("En el dado 3 te ha salido: %d%n",dado3);
+        int suma=0;
+        for (int i=1;i<=3;i++){
+            int dado=(int)(Math.random()*6)+1;
+            System.out.printf("En el dado %d te ha salido: %d%n",i,dado);
+            suma+=dado;
+        }
         System.out.printf("La suma de los dados es: %d%n",suma);
     }
 }
