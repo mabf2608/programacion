3b2196f3f2aa89dc75effdc8f3929bbb11b7004b
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-11-15 12:32:52 +0100

    Ejercicio13 Tema 6

--- CAP06EJS/src/Ejercicio13.java
+++ CAP06EJS/src/Ejercicio13.java
@@ -1,5 +1,20 @@
+/*
+*Ejercicio 13 | Tema 6
+*Tirada de dados
+*@author Miguel Ángel Bonilla Fernández 1ºDAM
+*/
 public class Ejercicio13 {
     public static void main(String[] args) {
-        
+        System.out.println();
+        boolean seguir=true;
+        while (seguir) {
+            int d1=(int)(Math.random()*6+1),d2=(int)(Math.random()*6+1);
+            System.out.printf("El resultado del dado 1 es: %d.%n",d1);
+            System.out.printf("El resultado del dado 2 es: %d.%n",d2);
+            if(d1==d2){
+                seguir=false;
+            }
+            System.out.println();
+        }
     }
 }
