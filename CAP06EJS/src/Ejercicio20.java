ab872285e3dd0e34dd84d634001d081f76b45eda
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-11-20 10:31:42 +0100

    Ejercicio 20 Completo Tema 6

---
+++ CAP06EJS/src/Ejercicio20.java
@@ -0,0 +1,34 @@
+/*
+*Ejercicio 20 | Tema 6
+*Cuba de agua
+*@author Miguel Ángel Bonilla Fernández 1ºDAM
+*/
+public class Ejercicio20 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.print("Introduzca la capacidad de la cuba en litros: ");
+        int cap=Integer.parseInt(System.console().readLine());
+        cap+=1;
+        int anch=6,j=0,litros=(int)(Math.random()*cap),dif=cap-litros;
+        for(int i=1;i<=cap;i++){
+            if (i<cap){
+                for(j=1;j<=anch;j++){
+                    if (j==1||j==anch){
+                        System.out.print("#");
+                    }else{
+                        if (i>=dif)
+                            System.out.print("=");
+                        else
+                            System.out.print(" ");
+                    }
+                }
+            }else{
+                for(j=1;j<=anch;j++){
+                    System.out.print("#");
+                }
+            }
+            System.out.println();
+        }
+        System.out.printf("La cuba tiene una capacidad de %d y contiene %d litros de agua.%n",cap-1,litros);
+    }
+}
