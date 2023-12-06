cba4a2de1888769c96874f2a943dae0afa9e7171
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-12-06 21:48:24 +0100

    Ejercicio 54 Tema 5 edit

--- CAP05EJS/src/Ejercicio54.java
+++ CAP05EJS/src/Ejercicio54.java
@@ -7,7 +7,7 @@ public class Ejercicio54 {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("Introduzca la altura de la figura: ");
-            int a=Integer.parseInt(System.console().readLine());
+        int a=Integer.parseInt(System.console().readLine());
         int i=0, j=0;
         for (i=0;i<=a;i++){
             System.out.println();
@@ -27,4 +27,3 @@ public class Ejercicio54 {
         }
     }
 }
-
