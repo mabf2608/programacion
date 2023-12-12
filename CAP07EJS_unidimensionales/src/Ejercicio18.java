98cd91ab72944d6b939aa51daf933b9a514d7af1
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-12-12 20:13:47 +0100

    Ejercicio 18 Tema 7

--- CAP07EJS_unidimensionales/src/Ejercicio18.java
+++ CAP07EJS_unidimensionales/src/Ejercicio18.java
@@ -1,15 +1,15 @@
 /*
 *Ejercicio 18 | Tema 7
-*Array forma alterna pares e impares.
+*Array forma alterna.
 *@author Miguel Ángel Bonilla Fernández 1ºDAM
 */
 public class Ejercicio18 {
-    public static boolean espar(int n){
-        boolean par=true;
-        if(n%2!=0){
-            par=false;
+    public static boolean esmenor_100(int n){
+        boolean menor_100=true;
+        if(n>100){
+            menor_100=false;
         }
-        return par;
+        return menor_100;
     }
     public static void tabla(int[] numero){
         int i;
@@ -36,19 +36,19 @@ public class Ejercicio18 {
     public static void main(String[] args) {
         System.out.println();
         int[] numero=new int[10];
-        int[] par=new int[10];
-        int[] nopar=new int[10];
-        int pares=0,nopares=0;
+        int[] menor_100=new int[10];
+        int[] mayor_100=new int[10];
+        int menores=0,mayores=0;
         int i;
         for(i=0;i<10;i++){
             System.out.printf("Introduce el numero: %d: ",i+1);
             numero[i]=(int)(Math.random()*201);
-            if (espar(numero[i])){
-                par[pares]=numero[i];
-                pares++;
+            if (esmenor_100(numero[i])){
+                menor_100[menores]=numero[i];
+                menores++;
             }else{
-                nopar[nopares]=numero[i];
-                nopares++;
+                mayor_100[mayores]=numero[i];
+                mayores++;
             }
         }
         System.out.println();
@@ -59,12 +59,12 @@ public class Ejercicio18 {
         int cont1=0;
         int cont2=0;
         for (i=0;i<10;i++){
-            if (i%2==0&&cont1<pares||nopares==cont2){;
-                numero[i]=par[cont1];
+            if (i%2==0&&cont1<menores||mayores==cont2){;
+                numero[i]=menor_100[cont1];
                 cont1++;
             }
-            if (i%2!=0&&cont2<nopares||pares<cont1){
-                numero[i]=nopar[cont2];
+            if (i%2!=0&&cont2<mayores||menores<cont1){
+                numero[i]=mayor_100[cont2];
                 cont2++;
             }
         }
