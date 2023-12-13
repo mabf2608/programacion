2e04bcd8ca706efff002029d8e1c9ae736ad9cbf
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-12-13 09:49:05 +0100

    Ejercicio5 Tema 7.2

---
+++ CAP07EJS_bidimensionales/src/Ejercicio5.java
@@ -0,0 +1,74 @@
+/*
+*Ejercicio 5 | Tema 7.1
+*Tabla matriz con máximo y mínimo
+*@author Miguel Ángel Bonilla Fernández 1ºDAM
+*/
+public class Ejercicio5 {
+    public static int[][] rellenar_matriz(int[][] numeros){
+        int nummax=1000;
+        int nummin=0;
+        int i;
+        int j;
+        for(i=0;i<6;i++){
+            for(j=0;j<10;j++){
+                numeros[i][j]=(int)(Math.random()*(nummax-nummin+1)+nummin);
+            }
+        }
+        return numeros;
+    }
+    public static void crear_tabla(int[][] numeros){
+        System.out.println();
+        int i;
+        int j;
+        for(i=-1;i<6;i++){
+            for(j=-1;j<10;j++){
+                if (i==-1&&j==0){
+                    System.out.printf("%6d",j);
+                }
+                if (i==-1&&j>0){
+                    System.out.printf("%8d",j);
+                }
+                if (i>=0&&j==-1){
+                    System.out.printf("%d",i);
+                }
+                if (i>=0&&j==0){
+                    System.out.printf("%6d",numeros[i][j]);
+                }
+                if(i>=0&&j>0){
+                        System.out.printf("%8d",numeros[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int[][] numeros=new int[6][10];
+        int i;
+        int j;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        int posicion_y_max=0;
+        int posicion_x_max=0;
+        int posicion_y_min=0;
+        int posicion_x_min=0;
+        rellenar_matriz(numeros);
+        for(i=0;i<6;i++){
+            for(j=0;j<10;j++){
+                if(numeros[i][j]>max){
+                    max=numeros[i][j];
+                    posicion_y_max=i;
+                    posicion_x_max=j;
+                }
+                if(numeros[i][j]<min){
+                    min=numeros[i][j];
+                    posicion_y_min=i;
+                    posicion_x_min=j;
+                }
+            }
+        }
+        crear_tabla(numeros);
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.printf("El máximo es %d y está en la fila %d, columna %d.%n",max,posicion_y_max,posicion_x_max);
+        System.out.printf("El mínimo es %d y está en la fila %d, columna %d.%n",min,posicion_y_min,posicion_x_min);
+    }
+}
