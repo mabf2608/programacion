6b0992967f7b6aca7f40a41248ff2b1ca2a87d9a
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2024-02-13 09:22:34 +0100

    Ejercicio 6 POO

---
+++ CAP09EJS/src/ejercicios/ejercicio6/usoTiempo.java
@@ -0,0 +1,20 @@
+package ejercicios.ejercicio6;
+
+public class usoTiempo {
+    public static void main(String[] args) {
+        System.out.println();
+
+        Tiempo t1=new Tiempo(10, 30, 14);
+        Tiempo t2=new Tiempo(5, 60, 04);
+        Tiempo t3=new Tiempo(1, 40, 55);
+
+        System.out.println(t1.getHora());
+        System.out.println(t2.getMinutos());
+        System.out.println(t3.getSegundos());
+
+        Tiempo.suma(t1, t2);
+        Tiempo.resta(t2, t3);
+
+        t1.toString();
+    }
+}
