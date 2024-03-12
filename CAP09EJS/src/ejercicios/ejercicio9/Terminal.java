30666644e3b4cd1832b21f76cffcc520c674229f
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2024-03-12 09:43:46 +0100

    Ejercicios 8 y 9 POO

---
+++ CAP09EJS/src/ejercicios/ejercicio9/Terminal.java
@@ -0,0 +1,35 @@
+package ejercicios.ejercicio9;
+
+public class Terminal {
+    protected int segundosConversacion;
+    protected String numero;
+
+    public Terminal(String numero){
+        this.numero=numero;
+    }
+
+    public void llama(Terminal x, int seg){
+        this.segundosConversacion+=seg;
+        x.segundosConversacion+=seg;
+    }
+
+    public String getNumero(){
+        return numero;
+    }
+
+    public void setNumero(String num){
+        numero=num;
+    }
+
+    public void setSegundosConversacion(int seg){
+        segundosConversacion=seg;
+    }
+
+    public int getSegundosConversacion(){
+        return segundosConversacion;
+    }
+    @Override
+    public String toString() {
+        return String.format("Nº %s - %ds de conversación.",numero,segundosConversacion);
+    }
+}
