3ecd2c904e36093020f039a94c7170a7c9f83c00
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2024-03-21 10:44:31 +0100

    Ejercicio 16 POO

---
+++ CAP09EJS/src/ejercicios/ejercicio16/usoLinea.java
@@ -0,0 +1,18 @@
+/*
+*Ejercicio 16 | Tema 9
+*Punto y Línea.
+*@author Miguel Ángel Bonilla Fernández 1ºDAM
+*/
+
+package ejercicios.ejercicio16;
+
+public class usoLinea {
+    public static void main(String[] args) {
+        Punto p1 = new Punto(4.21, 7.3);
+        Punto p2 = new Punto(-2, 1.66);
+
+        Linea l = new Linea(p1, p2);
+        
+        System.out.println(l);
+    }
+}
