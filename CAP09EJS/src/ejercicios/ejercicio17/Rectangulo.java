301c1fbbfcc98f382964ae1bd8f054693d229cba
mabf2608 <m.a.boni26@hotmail.com>
2024-03-23 20:48:55 +0100

    Ejercicio 17 POO

--- CAP09EJS/src/ejercicios/ejercicio17/Rectangulo.java
+++ CAP09EJS/src/ejercicios/ejercicio17/Rectangulo.java
@@ -1,36 +1,42 @@
 package ejercicios.ejercicio17;
 
-public class Piramide {
-    public static int piramidesCreadas;
-    private int alt;
+public class Rectangulo {
+    private int alt, bas;
+    public static int rectangulosCreados;
 
-    public Piramide(int alt){
+    public Rectangulo(int bas, int alt){
         this.alt=alt;
+        this.bas=bas;
     }
 
     public void setAlt(int alt){
         this.alt=alt;
     }
 
+    public void setBas(int bas){
+        this.bas=bas;
+    }
+
     public int getAlt(){
         return alt;
     }
 
-    public static int getPiramidesCreadas(){
-        return piramidesCreadas;
+    public int getBas(){
+        return bas;
+    }
+
+    public static int getRectangulosCreados(){
+        return rectangulosCreados;
     }
 
     @Override
     public String toString() {
-        piramidesCreadas++;
+        rectangulosCreados++;
         int i,j;
         String resultado="";
 
         for (i=1;i<=alt;i++){
-            for(j=1;j<=alt-i;j++){
-                resultado+=" ";
-            }
-            for(j=1;j<=(2*i)-1;j++){
+            for(j=1;j<=bas;j++){
                 resultado+="*";
             }
             resultado+="\n";
